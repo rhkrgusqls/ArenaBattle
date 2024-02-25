ea0d8f5c824c2b025115ab6962a95317c55545ab
rhkrgusqls <79958305+rhkrgusqls@users.noreply.github.com>
2024-02-26 08:01:05 +0900

    Origin

---
+++ Source/ArenaBattle/CharacterState/ABCharacterStateComponent.cpp
@@ -0,0 +1,58 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CharacterState/ABCharacterStateComponent.h"
+#include "GameData/ABGameSingleton.h"
+
+// Sets default values for this component's properties
+UABCharacterStateComponent::UABCharacterStateComponent()
+{
+	CurrentLevel = 0;
+}
+
+
+// Called when the game starts
+void UABCharacterStateComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	SetLevelStat(CurrentLevel);
+	SetHP(BaseStat.MaxHP);	
+}
+
+void UABCharacterStateComponent::SetHP(float NewHP)
+{
+	CurrentHP = FMath::Clamp(NewHP, 0.0f, BaseStat.MaxHP);
+
+	OnHpChanged.Broadcast(CurrentHP);
+}
+
+
+// Called every frame
+void UABCharacterStateComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+float UABCharacterStateComponent::ApplyDamage(float InDamage)
+{
+	float ActualDamage = FMath::Clamp(InDamage, 0.0f, InDamage);
+	
+	SetHP(CurrentHP - ActualDamage);
+	if (CurrentHP <= 0.0f)
+	{
+		OnHpZero.Broadcast();
+
+	}
+
+	return 0.0f;
+}
+
+void UABCharacterStateComponent::SetLevelStat(uint8 InNewLevel)
+{
+	CurrentLevel = FMath::Clamp(InNewLevel, 1, UABGameSingleton::Get().GetMaxLevel());
+	BaseStat = UABGameSingleton::Get().GetCharacterStatTable(CurrentLevel);
+}
+
